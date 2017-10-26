488dd380b6d5cb9bf8c79620150ddcb89e441f4a
Oskar Truffer <ot@studer-raimann.ch>
2017-10-26 11:57:10 +0200

    initialize ebook routes

---
+++ RESTController/extensions/ebook_v1/models/EBookModel.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace RESTController\extensions\eBook;
+
+//require_once(dirname(__DIR__) . '/models/EBookModel.php');
+require_once("./Customizing/global/plugins/Services/Repository/RepositoryObject/eBook/classes/class.ilObjeBook.php");
+require_once("./Customizing/global/plugins/Services/Repository/RepositoryObject/eBook/classes/class.ileBookConfig.php");
+
+use \RESTController\libs as Libs;
+
+class EBookModel  extends Libs\RESTModel {
+	/**
+	 * @var \ilDB
+	 */
+	protected $db;
+
+	/**
+	 * @var \ilAccessHandler
+	 */
+	protected $access;
+
+
+	public function __construct()
+	{
+		global $ilDB, $ilAccess;
+		Libs\RESTilias::loadIlUser();
+		Libs\RESTilias::initAccessHandling();
+		$this->db = $ilDB;
+		$this->access = $ilAccess;
+	}
+
+
+	/**
+	 * @param $user_id int
+	 *
+	 * @return mixed
+	 */
+	public function getEBooks($user_id) {
+		global $DIC;
+
+		$access = $DIC->access();
+		$config = new \ileBookConfig($DIC->database());
+		$root_ref = $config->get('library_root_ref_id');
+
+
+		$query = "
+				SELECT id, ref.ref_id, data.title, data.description, data.create_date, data.last_update FROM rep_robj_xebk_data
+				INNER JOIN object_data data ON data.obj_id = rep_robj_xebk_data.id 
+				INNER JOIN object_reference ref ON ref.obj_id = data.obj_id 
+				WHERE is_online = 1 AND ref.deleted is NULL 
+				";
+		$set = $this->db->query($query);
+		$books = [];
+		while($res = $this->db->fetchAssoc($set)) {
+			if(!$access->checkAccessOfUser($user_id, "read", "", $res['ref_id']))
+				continue;
+			$path = $this->createPath($res, $root_ref);
+			$res['id'] = (int) $res['id'];
+			$res['ref_id'] = (int) $res['ref_id'];
+			$res['path'] = $path;
+			// Path must contain root ref. otherwise we are not in scope.
+			if(0 == count(array_filter($path, function ($element) use ($root_ref) {
+				return $element['ref_id'] == $root_ref;
+			})))
+				continue;
+			$books[] = $res;
+		}
+		return $books;
+	}
+
+
+	/**
+	 * @param $res  array
+	 * @param $root_ref
+	 *
+	 * @return \array[]
+	 */
+	private function createPath($res, $root_ref) {
+		global $tree;
+		$path = $tree->getNodePath($res['ref_id']);
+		$path = array_map(function($element) {
+			return ["ref_id"=> $element['child'], "title" =>$element['title']];
+		}, $path);
+		$path_ids = array_map(function($e) {
+			return $e['ref_id'];
+		}, $path);
+		$slice_id = array_search($root_ref, $path_ids);
+		if($slice_id === false)
+			return [];
+		return array_slice($path, $slice_id, -1);
+	}
+
+
+	/**
+	 * @param $user_id
+	 * @param $ref_id
+	 *
+	 * @return string
+	 * @throws NoAccessException
+	 * @throws NoFileException
+	 */
+	public function getFilePathByRefId($user_id, $ref_id) {
+		global $DIC;
+
+		$access = $DIC->access();
+
+		if(!$access->checkAccessOfUser($user_id, "read", "", $ref_id))
+			throw new NoAccessException();
+
+		$object = new \ilObjeBook($ref_id);
+
+		if (!$object->hasFile())
+			throw new NoFileException();
+
+		return $object->getEncryptedFilePath();
+	}
+
+
+	public function getKeyByRefId($userId, $ref_id) {
+		global $DIC;
+
+		$access = $DIC->access();
+
+		if(!$access->checkAccessOfUser($userId, "read", "", $ref_id))
+			throw new NoAccessException();
+
+		$object = new \ilObjeBook($ref_id);
+
+		if (!$object->hasFile())
+			throw new NoFileException("There's no key to this ref id.");
+
+		return $object->getSecret();
+	}
+}
---
+++ RESTController/extensions/ebook_v1/routes/EBookRoutes.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace RESTController\extensions\eBook;
+
+require_once(dirname(__DIR__) . '/models/EBookModel.php');
+require_once("./Services/FileDelivery/classes/class.ilFileDelivery.php");
+require_once("./Customizing/global/plugins/Services/Repository/RepositoryObject/eBook/classes/class.ileBookAccessLog.php");
+
+
+use \RESTController\libs\RESTAuth as RESTAuth;
+use \RESTController\core\auth as Auth;
+use RESTController\RESTController;
+
+/** @var $app RESTController */
+/**
+ * Note: The additional OPTIONS request per route is needed due to CORS. Before sending an actual GET/POST request,
+ * the browser is sending an OPTIONS request to check if the origin (e.g. localhost) is allowed to perform
+ * cross origin site requests. The OPTIONS request is sent without Authorization headers und thus results in a 401 if
+ * the TOKEN middleware is active.
+ */
+$app->group('/v1/ebook', function () use ($app) {
+
+	/**
+	 * set CORS Headers
+	 */
+	$app->response->headers->set('Access-Control-Allow-Origin', '*');
+	$app->response->headers->set('Access-Control-Allow-Headers', 'Authorization,XDEBUG_SESSION,XDEBUG_SESSION_START');
+	$app->response->headers->set('Access-Control-Allow-Methods', 'GET,POST');
+
+	/**
+	 * GET all files
+	 */
+	$app->get('/', RESTAuth::checkAccess(RESTAuth::TOKEN), function() use ($app) {
+		$accessToken = $app->request->getToken();
+		$model = new EBookModel();
+		$userId = $accessToken->getUserId();
+		$app->response->body(json_encode($model->getEBooks($userId)));
+	});
+	$app->options('/', function() {});
+
+	/**
+	 * GET encoded file binary
+	 */
+	$app->get('/:refId/file', RESTAuth::checkAccess(RESTAuth::TOKEN), function($ref_id) use ($app) {
+		$accessToken = $app->request->getToken();
+		$model = new EBookModel();
+		$userId = $accessToken->getUserId();
+
+		try {
+			$filePath = $model->getFilePathByRefId($userId, $ref_id);
+		} catch (NoFileException $e) {
+			$app->halt(404, "No file uploaded yet.");
+		} catch (NoAccessException $e) {
+			$app->halt(401, "No access.");
+		}
+		/**
+		 * @var $ilClientIniFile ilIniFile
+		 */
+		require_once('./Services/FileDelivery/classes/class.ilFileDelivery.php');
+
+		$ilFileDelivery = new \ilFileDelivery($filePath);
+		$ilFileDelivery->setMimeType('application/pdf');
+		$ilFileDelivery->deliver();
+	});
+	$app->options('/:refId/file', function() {});
+
+	/**
+	 * GET key
+	 */
+	$app->get('/:refId/key', RESTAuth::checkAccess(RESTAuth::TOKEN), function($ref_id) use ($app) {
+		$accessToken = $app->request->getToken();
+		$model = new EBookModel();
+		$userId = $accessToken->getUserId();
+
+		try {
+			$key = $model->getKeyByRefId($userId, $ref_id);
+			$remote_address = $_SERVER['REMOTE_ADDR'];
+			$forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+			$access = new \ileBookAccessLog();
+			$access->setUserId($userId);
+			$access->setEbookId($ref_id);
+			$access->setRemoteAddress($remote_address);
+			$access->setXForwardedFor($forwarded_for);
+			$access->updateTimestamp();
+			$access->setAction(\ileBookAccessLog::ACTION_DOWNLOAD_TOKEN);
+//			print_r($access);exit;
+			$access->create();
+
+			$app->response->body(json_encode(["key" => $key]));
+		} catch (NoFileException $e) {
+			$app->halt(404, "No file uploaded yet.");
+		} catch (NoAccessException $e) {
+			$app->halt(401, "No access.");
+		}
+
+	});
+	$app->options('/:refId/file', function() {});
+
+	/**
+	 * GET if key is still valid
+	 *
+	 * returns success if the logged in user still has access to the ebook.
+	 */
+	$app->get('/:refId/validateKey', RESTAuth::checkAccess(RESTAuth::TOKEN), function($ref_id) use ($app) {
+		$accessToken = $app->request->getToken();
+		$model = new EBookModel();
+		$userId = $accessToken->getUserId();
+
+		try {
+			$model->getKeyByRefId($userId, $ref_id);
+			$app->response->body(json_encode(["success" => true]));
+		} catch (NoFileException $e) {
+			$app->halt(404, "No file uploaded yet.");
+		} catch (NoAccessException $e) {
+			$app->halt(401, "No access.");
+		}
+
+	});
+	$app->options('/:refId/validateKey', function() {});
+
+
+
+	//	$app->get('/objects/:refId', RESTAuth::checkAccess(RESTAuth::TOKEN), function($refId) use ($app) {
+//		$iliasApp = new ILIASAppModel();
+//		$accessToken = $app->request->getToken();
+//		$userId = $accessToken->getUserId();
+//		$app->response->headers->set('Content-Type', 'application/json');
+//		$recursive = $app->request->get('recursive');
+//		$data = ($recursive) ? $iliasApp->getChildrenRecursive($refId, $userId) : $iliasApp->getChildren($refId, $userId);
+//		$app->response->body(json_encode($data));
+//	});
+//
+//	$app->options('/objects/:refId', function() {});
+//
+//	$app->get('/files/:refId', RESTAuth::checkAccess(RESTAuth::TOKEN), function($refId) use ($app) {
+//		$iliasApp = new ILIASAppModel();
+//		$accessToken = $app->request->getToken();
+//		$userId = $accessToken->getUserId();
+//		$app->response->headers->set('Content-Type', 'application/json');
+//		$app->response->body(json_encode($iliasApp->getFileData($refId, $userId)));
+//	});
+//
+//	$app->options('/files/:refId', function() {});
+
+});
