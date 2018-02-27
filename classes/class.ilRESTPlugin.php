f5acf8e13835e096e68a3c136f3fef267df46043
Nicolas Schäfli <ns@studer-raimann.ch>
2018-02-27 09:23:33 +0100

    Add uninstallation routine for REST plugin

--- classes/class.ilRESTPlugin.php
+++ classes/class.ilRESTPlugin.php
@@ -8,7 +8,10 @@
  
  
 // Include core UIHook plugin slot class
-include_once('Services/UIComponent/classes/class.ilUserInterfaceHookPlugin.php');
+use RESTController\extensions\ILIASApp\V2\HashCacheEntry;
+
+require_once 'Services/UIComponent/classes/class.ilUserInterfaceHookPlugin.php';
+require_once 'Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/REST/RESTController/extensions/ilias_app_v2/services/FileHashing/entity/HashCacheEntry.php';
 
 
 /**
@@ -29,4 +32,28 @@ class ilRESTPlugin extends ilUserInterfaceHookPlugin
     function getPluginName() {
         return "REST";
     }
+
+
+	/**
+	 * Custom uninstall routine which drops all database tables of the plugin.
+	 * This method will not emit errors if the tables were not found, to
+	 * enable the uninstallation of damaged or inconsistent plugin setups.
+	 *
+	 * @return bool This uninstallation routine will always return true.
+	 */
+	protected function beforeUninstall() {
+    	global $DIC;
+
+    	$db = $DIC->database();
+    	$db->dropTable('ui_uihk_rest_config', false);
+    	$db->dropTable('ui_uihk_rest_client', false);
+    	$db->dropTable('ui_uihk_rest_perm', false);
+    	$db->dropTable('ui_uihk_rest_refresh', false);
+    	$db->dropTable('ui_uihk_rest_authcode', false);
+    	$db->dropTable('ui_uihk_rest_access', false);
+    	$db->dropTable('ui_uihk_rest_token', false);
+    	$db->dropTable(HashCacheEntry::returnDbTableName(), false);
+
+		return true;
+	}
 }
