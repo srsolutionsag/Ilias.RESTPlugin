9a33f040fbb65423f363b3754edf6da4e3cc4508
mschneiter <msc@studer-raimann.ch>
2019-12-05 16:21:37 +0100

    ILIASAPP-645 (version 1) farbwahl

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+
+## [1.8.4] - 2019-12-05
+### Added👍
+- ILIASAPP-645 (version 1) farbwahl
+- route v3 for theme colors
+- setting theme colors in the configuration page of the plugin
+
+## [1.8.3] - 2019-11-22
+### Added👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍
+- RP001 & RP002: Lernforttschritte abholen & hochschreiben
+- route v3 for files
+- checking access-rights for files
+
+### Fixed
+- array definition in RESTLib.php
+
+# Template
+## [v.v.v] - yyyy-mm-dd
+### Added👍
+- ...
+### Changed
+- ...
+### Fixed
+- ...
+### Removed
+- ...
--- RESTController/extensions/ilias_app_v3/models/ILIASAppModel.php
+++ RESTController/extensions/ilias_app_v3/models/ILIASAppModel.php
@@ -9,23 +9,23 @@ require_once('./Modules/File/classes/class.ilObjFile.php');
 
 final class ILIASAppModel extends Libs\RESTModel {
 
-	/**
-	 * @var \ilDB
-	 */
-	private $db;
+    /**
+     * @var \ilDB
+     */
+    private $db;
 
-	/**
-	 * @var \ilAccessHandler
-	 */
-	private $access;
+    /**
+     * @var \ilAccessHandler
+     */
+    private $access;
 
 
-	public function __construct() {
-		global $ilDB, $ilAccess;
-		Libs\RESTilias::loadIlUser();
-		$this->db = $ilDB;
-		$this->access = $ilAccess;
-	}
+    public function __construct() {
+        global $ilDB, $ilAccess;
+        Libs\RESTilias::loadIlUser();
+        $this->db = $ilDB;
+        $this->access = $ilAccess;
+    }
 
 
     /**
@@ -35,43 +35,43 @@ final class ILIASAppModel extends Libs\RESTModel {
      * @param $userId int
      * @return array
      */
-	public function getFileData($refId, $userId) {
-	    // check access
+    public function getFileData($refId, $userId) {
+        // check access
         if(!$this->isVisible($refId))
             return ["body" => new ErrorAnswer("Forbidden"), "status" => 403];
 
         // get file data
-		$file = new \ilObjFile($refId);
-		// file name
-		$fileName = mb_strtolower($file->getFileName());
-		$fileName = preg_replace('/[^a-z0-9\-_\.]+/', '', $fileName);
+        $file = new \ilObjFile($refId);
+        // file name
+        $fileName = mb_strtolower($file->getFileName());
+        $fileName = preg_replace('/[^a-z0-9\-_\.]+/', '', $fileName);
 
-		// learning progress
-		global $DIC;
-		$ilDB = $DIC['ilDB'];
-		$q = "SELECT status FROM ut_lp_marks WHERE obj_id=" . $file->getId() . " AND usr_id=" . $userId;
-		$ret = $ilDB->query($q);
-		$fileLearningProgress = boolval($ilDB->fetchAssoc($ret)["status"]);
+        // learning progress
+        global $DIC;
+        $ilDB = $DIC['ilDB'];
+        $q = "SELECT status FROM ut_lp_marks WHERE obj_id=" . $file->getId() . " AND usr_id=" . $userId;
+        $ret = $ilDB->query($q);
+        $fileLearningProgress = boolval($ilDB->fetchAssoc($ret)["status"]);
 
-		return ["body" => array(
-			'fileExtension' => $file->getFileExtension(),
-			'fileName' => $fileName,
-			'fileSize' => strval($file->getFileSize()),
-			'fileType' => $file->getFileType(),
-			'fileVersion' => strval($file->getVersion()),
-			'fileVersionDate' => $file->getLastUpdateDate(),
-			'fileLearningProgress' => $fileLearningProgress
-		)];
-	}
+        return ["body" => array(
+            'fileExtension' => $file->getFileExtension(),
+            'fileName' => $fileName,
+            'fileSize' => strval($file->getFileSize()),
+            'fileType' => $file->getFileType(),
+            'fileVersion' => strval($file->getVersion()),
+            'fileVersionDate' => $file->getLastUpdateDate(),
+            'fileLearningProgress' => $fileLearningProgress
+        )];
+    }
 
-	/**
-	 * sets the lp-status for a file with reference $refId and user $userId to completed
-	 *
-	 * @param $refId int
-	 * @param $userId int
+    /**
+     * sets the lp-status for a file with reference $refId and user $userId to completed
+     *
+     * @param $refId int
+     * @param $userId int
      * @return array
-	 */
-	public function setFileLearningProgressToDone($refId, $userId) {
+     */
+    public function setFileLearningProgressToDone($refId, $userId) {
         // check access
         if(!($this->isVisible($refId) && $this->isRead($refId)))
             return ["body" => new ErrorAnswer("Forbidden"), "status" => 403];
@@ -92,27 +92,45 @@ final class ILIASAppModel extends Libs\RESTModel {
         return ["body" => array("message" => "Learning progress was successfully set to done")];
     }
 
+    /**
+     * collects the parameters for the theming of the app
+     *
+     * @return array
+     */
+    public function getThemeData() {
+        global $DIC;
+        $ilDB = $DIC['ilDB'];
+        $q = "SELECT * FROM ui_uihk_pegasus_theme WHERE id=1";
+        $ret = $ilDB->query($q);
+        if($ret === false)
+            return ["body" => new ErrorAnswer("Internal Server Error"), "status" => 500];
+        $dat = $ilDB->fetchAssoc($ret);
 
-	/**
-	 * Checks the access right of the given $refId for visible permission.
-	 *
-	 * @param $refId int a ref_id to check the access
-	 *
-	 * @return bool true if the permission is visible, otherwise false
-	 */
-	private function isVisible($refId) {
-		return $this->access->checkAccess('visible', '', $refId);
-	}
+        return ["body" => array(
+            'themePrimaryColor' => $dat["primary_color"],
+            'themeContrastColor' => boolval($dat["contrast_color"])
+        )];
+    }
 
 
-	/**
-	 * Checks the access right of the given $refId for read permission.
-	 *
-	 * @param $refId int a ref_id to check the access
-	 *
-	 * @return bool true if the permission is read, otherwise false
-	 */
-	private function isRead($refId) {
-		return $this->access->checkAccess('read', '', $refId);
-	}
+    /**
+     * Checks the access right of the given $refId for visible permission.
+     *
+     * @param $refId int a ref_id to check the access
+     * @return bool true if the permission is visible, otherwise false
+     */
+    private function isVisible($refId) {
+        return $this->access->checkAccess('visible', '', $refId);
+    }
+
+
+    /**
+     * Checks the access right of the given $refId for read permission.
+     *
+     * @param $refId int a ref_id to check the access
+     * @return bool true if the permission is read, otherwise false
+     */
+    private function isRead($refId) {
+        return $this->access->checkAccess('read', '', $refId);
+    }
 }
